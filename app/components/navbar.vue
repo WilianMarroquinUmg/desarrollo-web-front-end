0721ecdc63e9c82422a63c0aee80d126ea70f7e6
CPborja83 <cborjam@miumg.edu.gt>
2024-09-30 16:45:40 -0600

    Update navbar.vue

--- app/components/navbar.vue
+++ app/components/navbar.vue
@@ -37,6 +37,7 @@ export default defineComponent({
             <a class="nav-link disabled" aria-disabled="true">Disabled</a>
           </li>
         </ul>
+        //definir busqueda
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
