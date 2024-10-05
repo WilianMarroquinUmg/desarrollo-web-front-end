878c0104e1a5d578d49f05d37847990bcf7a3a6f
elisadardon <elisanoemi15@outlook.com>
2024-10-05 11:51:56 -0600

    Primer commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+    'bootstrap-vue/dist/bootstrap-vue.css'
+    ],
+    plugins: [
+    { src: '~/plugins/bootstrap-vue.js', ssr: true }
+    ],
+
 })
