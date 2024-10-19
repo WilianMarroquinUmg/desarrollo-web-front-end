dda52fcddc4f5e60a934e2572165504f88a7b2e2
yeremy2002 <barrazolac@miumg.edu.gt>
2024-10-19 09:08:04 -0600

    commit

--- app/pages/Sample-Page.vue
+++ app/pages/Sample-Page.vue
@@ -6,7 +6,13 @@ import UiParentCard from '@/components/shared/UiParentCard.vue';
     <v-row>
         <v-col cols="12" md="12">
             <UiParentCard title="Sample Page"> 
+
+
                 <div class="pa-7 pt-1"><p class="text-body-1">This is a sample page </p></div>
+
+
+
+                
             </UiParentCard>
         </v-col>
     </v-row>
