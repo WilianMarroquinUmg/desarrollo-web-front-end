a5bb55cb4c7e3b43079e8e0d9a2026c04e2cdf1a
WilianMarroquin <githubwiliangit@gmail.com>
2024-10-22 18:52:31 -0600

    Configurando el archivo nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,9 +32,7 @@ srcDir: 'app',
 
   sanctum: {
     mode: 'token',
-    baseUrl: 'http://back-pajas-agua-desarrollo-web.test/',
-    // baseUrl: 'http://desarrollo-web-back-end.local/',
-    // baseUrl: 'http://base-backend-with-laravel11.test/',
+    baseUrl: 'https://naranjobakend.lat/',
     userStateKey: 'sanctum.user.identity',
     redirectIfAuthenticated: true,
     redirectIfUnauthenticated: true,
@@ -63,83 +61,4 @@ srcDir: 'app',
     },
     logLevel: 3,
   }
-
-
 })
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
-// export default defineNuxtConfig({
-//   compatibilityDate: '2024-04-03',
-//   devtools: { enabled: true },
-//     modules: ["@nuxt/ui", "nuxt-auth-sanctum"],
-//
-//   srcDir: 'app',
-//   ssr: false,
-//
-//   typescript: {
-//     shim: false
-//   },
-//
-//   build: {
-//     transpile: ["vuetify"],
-//   },
-//
-//   vite: {
-//     define: {
-//       "process.env.DEBUG": false,
-//     },
-//   },
-//
-//   nitro: {
-//     serveStatic: true,
-//   },
-//
-//   devServerHandlers: [],
-//
-//   hooks: {
-//   },
-//
-//   sanctum: {
-//     mode : 'token',
-//     baseUrl: 'http://base-backend-with-laravel11.test/',
-//     userStateKey: 'sanctum.user.identity',
-//
-//     redirectIfAuthenticated: true,
-//     redirectIfUnauthenticated: true,
-//
-//     endpoints: {
-//       csrf: '/sanctum/csrf-cookie',
-//       login: 'api/auth/login',
-//       logout: 'api/auth/logout',
-//       user: '/api/user' // Endpoint para obtener el usuario autenticado
-//     },
-//
-//     client: {
-//       retry: false,
-//     },
-//
-//     csrf: {
-//       cookie: 'XSRF-TOKEN',
-//       header: 'X-XSRF-TOKEN',
-//     },
-//
-//     redirect: {
-//       keepRequestedRoute: false,
-//       onLogin: '/',
-//       onLogout: '/',
-//       onAuthOnly: '/auth/Login',
-//       onGuestOnly: '/',
-//     },
-//
-//     globalMiddleware: {
-//       enabled: false,
-//       allow404WithoutAuth: true,
-//     },
-//
-//     globalMiddleware: {
-//       enabled: true,
-//     },
-//     logLevel: 3,
-//   },
-//
-// })
